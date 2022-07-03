261c6892810c0435e3e86ebbb1149647ae25139f
eryn L. K <eryn@eryn.io>
2022-07-02 23:32:38 -0400

    update hydrateAutomaticSize for new behavior

--- src/hydrateAutomaticSize.lua
+++ src/hydrateAutomaticSize.lua
@@ -1,15 +1,6 @@
 local CollectionService = game:GetService("CollectionService")
 local automaticSize = require(script.Parent.automaticSize)
 
-local function hydrate(instance)
-	local axisName = instance:GetAttribute("axis")
-
-	automaticSize(instance, {
-		maxSize = instance:GetAttribute("maxSize"),
-		axis = if axisName then Enum.AutomaticSize[axisName] else nil,
-	})
-end
-
 --[=[
 	Applies automatic sizing to any current or future instances in the DataModel that are tagged with
 	`"PlasmaAutomaticSize"`. Attributes `axis` (string) and `maxSize` (UDim2 or Vector2) are allowed.
@@ -21,11 +12,11 @@ end
 ]=]
 local function hydrateAutomaticSize()
 	for _, instance in CollectionService:GetTagged("PlasmaAutomaticSize") do
-		hydrate(instance)
+		automaticSize(instance)
 	end
 
 	return CollectionService:GetInstanceAddedSignal("PlasmaAutomaticSize"):Connect(function(instance)
-		task.defer(hydrate, instance) -- instance added signal fires before children are added
+		task.defer(automaticSize, instance) -- instance added signal fires before children are added
 	end)
 end
 
